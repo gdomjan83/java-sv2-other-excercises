9cf0c739e20c20ce146d129e523fcd3ae41fd7e7
J_C <gomjan83@gmail.com>
2021-11-20 12:20:56 +0100

    second excercise finished

---
+++ src/main/java/potvizsga2/everysecond/EverySecond.java
@@ -0,0 +1,12 @@
+package potvizsga2.everysecond;
+
+public class EverySecond {
+
+    public String getEverySecondLetter(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i += 2) {
+            sb.append(s.charAt(i));
+        }
+        return new String(sb);
+    }
+}
