514f0372f01eca714edb04194c98ab963e8e7517
J_C <gomjan83@gmail.com>
2022-03-06 20:53:55 +0100

    zarovizsga potpotvizsga init

---
+++ src/test/java/potvizsga8/bistros/AddressTest.java
@@ -0,0 +1,18 @@
+//package potvizsga8.bistros;
+//
+//import org.junit.jupiter.api.Test;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//class AddressTest {
+//
+//
+//    @Test
+//    void createAddressTest(){
+//        Address address = new Address("Kossuth",18);
+//
+//        assertEquals("Kossuth",address.getStreet());
+//        assertEquals(18,address.getNumber());
+//    }
+//
+//}
